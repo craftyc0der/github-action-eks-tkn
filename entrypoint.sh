2263d56ca8045563e369beb6ebea3c97ddf05ee0
chisom.igwe <chisom.a.igwe@gmail.com>
2022-01-13 18:22:27 +0000

    fixup

--- entrypoint.sh
+++ entrypoint.sh
@@ -82,7 +82,7 @@ tkn_output=$(tkn task start --labels "REPO=${REPO}" --labels "GITHUB_SHA=${GITHU
 echo "==========================="
 echo $tkn_output
 echo "==========================="
-tkn taskrun logs $tkn_output -n ${INPUT_NAMESPACE}
+tkn taskrun logs ${tkn_output} -n ${INPUT_NAMESPACE}
 echo "==========================="
 
 
