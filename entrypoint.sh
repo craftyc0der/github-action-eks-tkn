239378025fc336932032d6b79f1f63c3f3b93f84
chisom.igwe <chisom.a.igwe@gmail.com>
2022-01-13 18:05:13 +0000

    fixup

--- entrypoint.sh
+++ entrypoint.sh
@@ -78,7 +78,7 @@ echo -e "\033[36mExecuting tkn\033[0m"
 status=$?
 REPO="${GITHUB_REPOSITORY##*/}"
 
-tkn_output=$(tkn task start --labels "REPO=${REPO}" --labels "GITHUB_SHA=${GITHUB_SHA}" ${PTARG} ${SAARG} -n ${INPUT_NAMESPACE} ${INPUT_TASK} $INPUT_ARGS --output json) 
+tkn_output=$(tkn task start --labels "REPO=${REPO}" --labels "GITHUB_SHA=${GITHUB_SHA}" ${PTARG} ${SAARG} -n ${INPUT_NAMESPACE} ${INPUT_TASK} $INPUT_ARGS --output json | jq ".metadata") 
 echo "==========================="
 echo $tkn_output
 echo "==========================="
